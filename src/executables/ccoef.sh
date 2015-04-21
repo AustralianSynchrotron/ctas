7d344a04ef3c46697465db6e916719bd82a91a3f
antonmx <antonmx@gmail.com>
2015-04-21 12:13:05 +1000

    modified:   src/common/common.cpp 	modified:   src/executables/ccoef.sh 	modified:   src/executables/ct-vol.cpp

--- src/executables/ccoef.sh
+++ src/executables/ccoef.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-read h1 c1 h2 c2 ;
+h1="$1"
+c1="$2"
+h2="$3"
+c2="$4"
+
+
 
 acof=$( echo "scale=8; ( $c1 - $c2 ) / ( $h1 - $h2 ) " | bc  )
 bcof=$( echo "scale=8; $c1 - $h1 * ( $c1 - $c2 ) / ( $h1 - $h2 ) " | bc  )

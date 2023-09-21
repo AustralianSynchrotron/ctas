a69bdae28c5a876cc5324d3ec6c4bdfa1946bbc7
_ <_>
2023-09-21 21:30:52 +1000

    modified:   ../src/executables/ct.cpp

--- src/executables/ct.cpp
+++ src/executables/ct.cpp
@@ -226,8 +226,8 @@ public:
         delete pntr;
         pntr=0;
       } } ;
+    for (auto celem : recs) if (celem.second != &canonRec) dlnl(celem.second);
     for (auto celem : rings) dlnl(celem.second);
-    for (auto celem : recs)  dlnl(celem.second);
     for (auto celem : imaps) dlnl(celem.second);
     for (auto celem : omaps) dlnl(celem.second);
   }

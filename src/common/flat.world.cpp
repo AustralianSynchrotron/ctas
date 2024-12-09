df306c3533bf8a8541b304e8cf230aeb65301b77
_ <_>
2024-12-09 15:45:11 +1100

    new file:   ../src/common/flat.world.cpp 	new file:   ../src/common/flat.world.h

---
+++ src/common/flat.world.cpp
@@ -0,0 +1,133 @@
+
+#include "flat.world.h"
+
+
+
+
+Shape<2> shapeMe(std::vector<Shape<2>> shs) {
+  Shape<2> firstSh(0,0);
+  for (int idx=0 ; idx < shs.size() ; idx++) {
+    if ( ! size(firstSh) )
+      firstSh = shs[idx];
+    if ( size(shs[idx]) && shs[idx] != firstSh )
+      throw_error("FlatFieldProc", "Non matching init shapes.");
+  }
+  return firstSh;
+}
+
+
+#ifdef ONGPU
+
+cl_program FlatFieldProc::ffProgram = 0;
+
+FlatFieldProc::FlatFieldProc( const Map & _bg, const Map & _df
+                            , const Map & _dg, const Map & _ms)
+  : sh( shapeMe( {_bg.shape(), _df.shape(), _dg.shape(), _ms.shape()} ) )
+  , io( area(sh) ? clAllocArray<float>(area(sh)) : 0)
+  , bg( _bg.size() ? blitz2cl(_bg, CL_MEM_READ_ONLY) : 0)
+  , df( _df.size() ? blitz2cl(_df, CL_MEM_READ_ONLY) : 0)
+  , dg( _dg.size() ? blitz2cl(_dg, CL_MEM_READ_ONLY) : 0)
+  , ms( _ms.size() ? blitz2cl(_ms, CL_MEM_READ_ONLY) : 0)
+{
+  if (!area(sh))
+    return;
+  static const std::string oclsrc = {
+    #include "ff.cl.includeme"
+  };
+  ffProgram = initProgram(oclsrc, ffProgram, "Flat field on OCL");
+  kernel(ffProgram, "ffm", false);
+  kernel.setArg(0, area(sh));
+  kernel.setArg(1, io());
+  kernel.setArg(2, bg());
+  kernel.setArg(3, df());
+  kernel.setArg(4, dg());
+  kernel.setArg(5, ms());
+}
+
+
+FlatFieldProc::FlatFieldProc(const FlatFieldProc & other)
+  : sh(other.sh)
+  , io( area(sh) ? clAllocArray<float>(area(sh)) : 0)
+  , bg(other.bg)
+  , df(other.df)
+  , dg(other.dg)
+  , ms(other.ms)
+{
+  if (!area(sh))
+    return;
+  kernel(ffProgram, "ffm", false);
+  kernel.setArg(0, area(sh));
+  kernel.setArg(1, io());
+  kernel.setArg(2, bg());
+  kernel.setArg(3, df());
+  kernel.setArg(4, dg());
+  kernel.setArg(5, ms());
+}
+
+
+bool FlatFieldProc::execute(const Map & _io) {
+  if ( !io() || !kernel )
+    return 0;
+  if (_io.shape() != sh)
+    throw_error("FlatCL", "Non matching input shape");
+  return blitz2cl(_io, io()) && kernel.exec(_io.size()) == CL_SUCCESS;
+}
+
+
+#else // ONGPU
+
+
+FlatFieldProc::FlatFieldProc( const Map & _bg, const Map & _df
+                            , const Map & _dg, const Map & _ms)
+  : sh( shapeMe( {_bg.shape(), _df.shape(), _dg.shape(), _ms.shape()} ) )
+  , df(_df)
+  , bga(_bg.shape())
+{
+  if (!size(sh))
+    return;
+  if (!_bg.size()) {
+    bga = 1.0;
+    return;
+  }
+
+  bga = _bg;
+  if (_dg.size())
+    bga -= _dg;
+  else if (_df.size())
+    bga -= _df;
+
+  for (ssize_t ycur = 0 ; ycur < sh(0) ; ycur++ )
+    for (ssize_t xcur = 0 ; xcur < sh(1) ; xcur++ ) {
+        float & val = bga(ycur,xcur);
+        if (_ms.size() && _ms(ycur,xcur)==0.0)
+          val = 0.0;
+        else if (val!=0.0)
+          val = 1.0/val;
+      }
+}
+
+
+FlatFieldProc::FlatFieldProc(const FlatFieldProc & other)
+  : sh(other.sh)
+  , df(other.df)
+  , bga(other.bga)
+{
+}
+
+
+Map & FlatFieldProc::process(Map & _io) const {
+  if (!size(sh))
+    return _io;
+  if (_io.shape() != sh)
+    throw_error("FlatFieldProc", "Non matching input shape");
+  if (df.size())
+    _io -= df;
+  if (bga.size())
+    _io *= bga;
+  return _io;
+}
+
+
+
+
+#endif // ONGPU
---
+++ src/common/flat.world.h
@@ -0,0 +1,224 @@
+/******************************************************************************
+ *   Copyright (C) 2007 by Anton Maksimenko                                   *
+ *   antonmx@post.kek.jp                                                      *
+ *                                                                            *
+ *   This program is free software; you can redistribute it and/or modify     *
+ *   it under the terms of the GNU General Public License as published by     *
+ *   the Free Software Foundation; either version 2 of the License, or        *
+ *   (at your option) any later version.                                      *
+ *                                                                            *
+ *   This program is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *   GNU General Public License for more details.                             *
+ *                                                                            *
+ *   You should have received a copy of the GNU General Public License        *
+ *   along with this program; if not, write to the                            *
+ *   Free Software Foundation, Inc.,                                          *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                *
+ ******************************************************************************/
+
+///
+/// @file
+/// @author antonmx <antonmx@gmail.com>
+/// @date   Thu Jul 17 12:58:59 2008
+///
+/// @brief Functions needed for flatfield correction.
+///
+
+
+#ifndef _H_BACKGROUND_H_
+#define _H_BACKGROUND_H_
+
+#include "matrix.world.h"
+
+
+
+
+//#define ONGPU
+#ifdef ONGPU
+#include <clFFT.h>
+#endif
+
+
+class FlatFieldProc {
+public:
+  const Shape<2> sh; // must be first
+private:
+  #ifdef ONGPU
+  static cl_program ffProgram;
+  CLkernel kernel;
+  CLmem   io;
+  const CLmem bg;
+  const CLmem df;
+  const CLmem dg;
+  #else // ONGPU
+  Map df;
+  Map bga;
+  #endif // ONGPU
+public:
+  FlatFieldProc( const Map & _bg, const Map & _df
+               , const Map & _dg, const Map & _ms);
+  FlatFieldProc(const FlatFieldProc & other);
+  FlatFieldProc(){};
+
+  explicit operator bool() const {return size(sh) && (df.size() || bga.size());}
+
+  #ifdef ONGPU
+  bool execute(const Map & _io);
+  inline Map & process(Map & _io) {
+    if (execute(_io))
+      cl2blitz(io(), _io);
+    return _io;
+  }
+  #else //ONGPU
+  Map & process(Map & _io) const;
+  #endif //ONGPU
+
+};
+
+
+
+
+/// \defgroup ff Flat field correction.
+///
+/// Functions which perform the flat field correction.
+///
+/// @{
+
+/// Performs the flat image subtraction.
+///
+/// Would be used only as the blitz::Array function.
+///
+/// @param fg foreground intensity.
+/// @param bg background intensity.
+/// @param dc dark current intensity.
+/// @param zer value to output on dc >= bg
+///
+/// @return intensity of the pixel with the subtracted background.
+///
+static inline float
+flatfield(float fg, float bg, float dc, float gf=-1, float zer=0.0){
+  if ( gf < 0 ) gf = dc;
+  if ( gf >= bg ) return zer;
+  else if (dc >= fg ) return 0.0;
+  else return (fg-dc)/(bg-gf);
+}
+
+
+/// Adds two arrays with weights.
+///
+/// @param result resulting array.
+/// @param in1 first array.
+/// @param in2 second array.
+/// @param ratio weight of the first array.
+///
+static inline Map &
+weighted(Map & result, const Map & in1, const Map & in2, float ratio=0.5) {
+  if      ( ratio <= 0.0 ) result.reference(in2);
+  else if ( ratio >= 1.0 ) result.reference(in1);
+  else if ( in1.shape() != in2.shape() )
+    throw_error("Weighted array addition", "Input arrays are of different size.");
+  else {
+    result.resize(in1.shape());
+    result = in1*ratio + in2*(1-ratio) ;
+  }
+  return result;
+}
+
+
+
+/// Performs the background subtraction on arrays.
+///
+/// @param result resulting array.
+/// @param fg foreground intensity.
+/// @param bg background intensity.
+///
+static inline void
+flatfield(Map & result, const Map & fg, const Map & bg){
+  if ( ! bg.size() ) {
+    if ( &fg != &result )  // arrays are not the same
+      result.reference(fg);
+    return;
+  }
+  const Shape<2> sh=fg.shape();
+  if( sh != bg.shape() )
+    throw_error("flat field", "Different shapes of the input arrays.");
+  if( sh != result.shape() )
+    result.resize(sh);
+  int sz=0;
+  for (ssize_t icur=0; icur<sh(0); icur++)
+    for (ssize_t jcur=0; jcur<sh(1); jcur++)
+      result(icur,jcur) = flatfield( fg(icur,jcur), bg(icur,jcur), 0.0, 0.0, 0.0);
+}
+
+
+
+
+
+
+/// Performs the flat field subtraction on arrays.
+///
+/// @param result resulting array.
+/// @param fg foreground intensity.
+/// @param bg background intensity.
+/// @param dc dark current intensity.
+/// @param zer value to output on dc >= bg
+///
+static inline void
+flatfield(Map & result, const Map & fg, const Map & bg, const Map & dc, const Map & gf, const float zer=1.0){
+  if ( ! dc.size() ) {
+    flatfield(result, fg, bg);
+    return;
+  }
+  if ( ! gf.size() ) {
+    flatfield(result, fg, bg, dc, dc, zer);
+    return;
+  }
+
+  const Shape<2> sh=fg.shape();
+  if( sh != bg.shape() || sh != dc.shape() || sh != gf.shape() )
+    throw_error("flat field", "Different shapes of the input arrays.");
+  if( sh != result.shape() )
+    result.resize(sh);
+  for (ssize_t icur=0; icur<sh(0); icur++)
+    for (ssize_t jcur=0; jcur<sh(1); jcur++)
+      result(icur,jcur) = flatfield( fg(icur,jcur), bg(icur,jcur), dc(icur,jcur), gf(icur,jcur), zer);
+}
+
+
+
+
+
+namespace blitz {
+
+/// Cut out values greater that 1.0.
+///
+/// Would be used only as the blitz::Array function.
+///
+/// @param x value to cut.
+///
+/// @return x if it is less than 1.0, 1.0 otherwise.
+///
+static inline float
+cutone(float x){
+  return x>1.0 ? 1.0 : x ;
+}
+
+/// \cond
+BZ_DECLARE_FUNCTION(cutone);
+/// \endcond
+
+}
+
+
+/// Description of the option giving the --idealworld option.
+const std::string IdealWorldOptionDesc =
+  "In the ideal absorption contrast the value of the background always"
+  " exceeds the respective value of the foreground"
+  " (i.e. foreground/background <= 1). If this option is used, the"
+  " points where the ratio exceeds 1, are set to 1.";
+
+/// @}
+
+#endif // _H_BACKGROUND_H_
